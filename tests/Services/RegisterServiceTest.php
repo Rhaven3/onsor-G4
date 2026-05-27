6c2facdf49ff24de7459276dcbffb3a1c8cd48eb
mbourret2026 <bourret57@gmail.com>
2026-05-27 12:24:47 +0200

    excel

--- tests/Services/RegisterServiceTest.php
+++ tests/Services/RegisterServiceTest.php
@@ -4,19 +4,26 @@ namespace App\Tests\Services;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use App\Services\UserService;
 use PHPUnit\Framework\TestCase;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 class RegisterServiceTest extends TestCase
 {
     public function testRegister(string $email, string $username, string $first_name, string $last_name, string $mobile): void
     {
-        $userRepository = $this->createMock(UserRepository::class);
-        $userRepository->method('findOneBy')->willReturn(null);
 
-        $user = new User();
-        $this->assertEquals('test@example.com', $user->getEmail());
+
+        $registrationData = [
+            'email'    => 'test@mail.com',
+            'username' => 'testuser',
+            'first'
+            'password' => '123456',
+
+            // autres champs selon ton entité...
+        ];
+
 
 
 
