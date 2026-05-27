2f6ea4c5d6447ccac19829395b8feabeb97a5157
amusenton2026 <arthur.musenton2026@campus-eni.fr>
2026-05-27 11:49:24 +0200

    avancé pop up annuler

---
+++ assets/controllers/popup_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static values = {
+        url: String
+    }
+
+    open(event) {
+        event.preventDefault();
+
+        let saisie = prompt("Veuillez le message d'annulation :");
+
+        if (saisie !== null && saisie.trim() !== "") {
+            const urlFinale = `${this.urlValue}?saisie=${encodeURIComponent(saisie)}`;
+
+            window.location.href = urlFinale;
+        }
+    }
+}
--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -106,13 +106,19 @@ final class TripController extends AbstractController
 
     #[Route('/{id}/cancel', name: 'cancel')]
     #[IsGranted("ROLE_USER")]
-    public function cancel(int $id): Response
+    public function cancel(int $id,Request $request): Response
     {
+
+        $valeurSaisie = $request->query->get('saisie');
+
         $trip = $this->tripService->find($id);
         if ($this->getUser() == $trip->getOrganisator() || $this->getUser()->getRoles()) {
-            $this->tripService->cancel($id);
+            if ($valeurSaisie ) {
+                $this->tripService->cancel($id);
+            }
         }
         return $this->redirectToRoute('trip_detail', ['id' => $id]);
+
     }
 
     #[Route('/{id}/publish', name: 'publish')]
