9cd01ae345da7af660ab64628fcba30ddb857ff1
mrobert2026 <matthieu.robert2026@campus-eni.fr>
2026-05-28 14:59:40 +0200

    M015 - Mise en place de la feature des FlashMessage. Changement d'organisation dans l'appel des fichiers JS.
    + Ajout des différents flash messages dans les controllers.
    + Ajout des classes d'écouteur de Login et Logout.
    + Ajout de l'affichage des flash messages sur le template de base.
    + CSS personnalisé pour l'animation des flash message
    + Ajout d'un fichier JS pour la gestion de suppression après animation des flash messages.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,8 +40,10 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
-
+            $this->addFlash('success', 'Utilisateur créé avec succès');
             return $this->redirectToRoute('app_home');
+        } elseif ($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('error', 'Une erreur est survenue');
         }
 
         return $this->render('registration/register.html.twig', [
