6679ac6ce580922411557616bc8a0b4be0258fca
Theo FONTAINE <theo.fontaine610@campus-eni.fr>
2026-05-28 16:46:50 +0200

    Correction du guard de publish

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -146,7 +146,7 @@ final class TripController extends AbstractController
     }
 
     #[Route('/{id}/publish', name: 'publish')]
-    #[IsGranted("WISH_PUBLISH")]
+    #[IsGranted("TRIP_PUBLISH")]
     public function publish(int $id): Response
     {
         $trip = $this->tripService->find($id);
